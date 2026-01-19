eebfd21bd4ccd4bdf41f0e0fe8263a9a0638d9d5
MichealTeron <116704880+MichealTeron@users.noreply.github.com>
2026-01-19 14:43:16 +0900

    Update __init__.py

--- src/tft_physics/__init__.py
+++ src/tft_physics/__init__.py
@@ -1,1 +1,12 @@
+from __future__ import annotations
+
+__all__ = [
+    "Config",
+    "DatasetSpec",
+    "TrainSpec",
+]
+
+from .config import Config
+from .data import DatasetSpec
+from .train import TrainSpec
 
